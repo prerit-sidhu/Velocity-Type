b98a539cb65e1c59112ad8fda1179388a40ef2fd
Prerit Sidhu <preritsidhu@gmail.com>
2025-09-02 20:22:32 +0000

    would it be a nice choice to add sensitive details into .env file so we

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -7,13 +7,13 @@ import { getAuth, Auth } from 'firebase/auth';
 import { getFirestore, Firestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  "projectId": "velocitytype",
-  "appId": "1:446119994650:web:90d6b55101d7d8902d78a3",
-  "storageBucket": "velocitytype.firebasestorage.app",
-  "apiKey": "AIzaSyDATHcpSp8ZpmgT8WK70YINfFTng40gt5I",
-  "authDomain": "velocitytype.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "446119994650"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 function initializeFirebase(config: any): { app: FirebaseApp, auth: Auth, db: Firestore } {
