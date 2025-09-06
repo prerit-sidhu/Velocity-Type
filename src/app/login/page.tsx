5e5acf953ec9a8bca01edb5a5b7785c68d1b6417
Prerit Sidhu <preritsidhu@gmail.com>
2025-09-06 06:43:19 +0000

    I've also encountered one issue that when i choose sign up option, in sm

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,7 +2,7 @@ import { LoginForm } from '@/components/login-form';
 
 export default function LoginPage() {
   return (
-    <div className="container flex min-h-screen flex-col items-center justify-center -mt-14">
+    <div className="container flex min-h-[calc(100vh-56px)] flex-col items-center justify-center py-8">
       <LoginForm />
     </div>
   );
